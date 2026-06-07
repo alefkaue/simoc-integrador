6c349790df4e4643354ccaf207a5a44210052c91
Alef Santos <167534821+alefkaue@users.noreply.github.com>
2026-06-07 02:09:22 -0300

    Update injetar_av.py

--- injetar_av.py
+++ injetar_av.py
@@ -4,21 +4,23 @@ BROKER = "10.10.100.100"
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 client.connect(BROKER, 1883, 60)
 
-for nivel in [90, 80, 70, 50, 20]:
-    payload = {
-        "pumpId": "PUMP001",
-        "capacityCurrent": int(5000 * (nivel / 100)),
-        "capacityTotal": 5000,
-        "capacityPercentage": str(float(nivel)),
-        "location": "Terminal Leste",
-        "fuelType": "Diesel S-10",
-        "timestamp": int(time.time() * 1000)
-    }
-    client.publish("fuel-pumps/data", json.dumps(payload))
-    print(f"Nivel: {nivel}%")
-    time.sleep(1) 
+print("[-] Transmissão contínua iniciada. Olhe o outro terminal!")
 
-order = {"pumpId": "PUMP003", "currentLevel": 5}
-client.publish("fuel-pumps/refill-request", json.dumps(order))
-print("Ordem falsa enviada para PUMP003")
-client.disconnect()
+try:
+    while True:
+        for nivel in [95, 80, 65, 40, 15]:
+            payload = {
+                "pumpId": "PUMP001",
+                "capacityCurrent": int(5000 * (nivel / 100)),
+                "capacityTotal": 5000,
+                "capacityPercentage": str(float(nivel)),
+                "location": "Terminal Leste",
+                "fuelType": "Diesel S-10",
+                "timestamp": int(time.time() * 1000)
+            }
+            client.publish("fuel-pumps/data", json.dumps(payload))
+            print(f"[+] Enviado nível: {nivel}%")
+            time.sleep(2)  # Envia a cada 2 segundos para dar tempo de ler
+except KeyboardInterrupt:
+    print("\n[-] Parando simulação...")
+    client.disconnect()
